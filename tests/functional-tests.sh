0220089a8a924328bb794cf6f40c375da8f7f59c
Erwan Velu <erwan@redhat.com>
2017-12-13 18:24:22 +0100

    tests: Code refactoring
    Making the functional_tests easier to read & maintain by:
    - deleting duplicated code
    - make a cn helper to get a very short command
    - generate tempfiles with mktemp

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -1,291 +1,158 @@
 #!/bin/bash
 # Functional tests
+err_file=""
+tmp_dir=/tmp
 
-function start {
-  err_file=/tmp/err_cn_start
-  ./cn start -d /tmp >"$err_file" 2>&1
+function fatal() {
+  echo "$@"
+  if [ -e $err_file ]; then
+    cat $err_file
+    deleteFile $err_file
+  fi
+  exit 1
+}
+
+function getTempFile() {
+  filename=$tmp_dir/$1.XXXXX
+  filename=$(mktemp $filename) &>/dev/null || fatal "Cannot create $filename"
+  echo $filename
+}
+
+function deleteFile() {
+  if [ -e "$1" ]; then
+    rm -f $1 || fatal "Cannot delete $1"
+  fi
+}
+
+function runCn() {
+  caller=${FUNCNAME[1]}
+  err_file=$(getTempFile $caller)
+  ./cn "$@" &>"$err_file"
   if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
+    printf '%-20s : SUCCESS\n' ${caller}
   else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
+    printf '%-20s : ERROR\n' ${caller}
+    fatal
     exit 1
   fi
-  rm -f "$err_file"
+  deleteFile $err_file
+}
+
+function start {
+  runCn start -d $tmp_dir
 }
 
 function help {
-  err_file=/tmp/err_cn_help
-  ./cn -h >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn -h
 }
 
 function stop {
-  err_file=/tmp/err_cn_stop
-  ./cn stop >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn stop
 }
 
 function status {
-  err_file=/tmp/err_cn_status
-  ./cn status >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn status
 }
 
 function restart {
-  err_file=/tmp/err_cn_restart
-  ./cn restart >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn restart
 }
 
 function logs {
-  err_file=/tmp/err_cn_logs
-  ./cn logs >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn logs
 }
 
 function purge {
-  err_file=/tmp/err_cn_purge
-  ./cn purge --yes-i-am-sure >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn purge --yes-i-am-sure
 }
 
 function update {
-  err_file=/tmp/err_cn_update
-  ./cn update >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn update
 }
 
 function version {
-  err_file=/tmp/err_cn_version
-  ./cn version >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn version
 }
 
 function s3_mb {
-  err_file=/tmp/err_cn_s3_mb
-  ./cn s3 mb aaa >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 mb aaa
 }
 
 function s3_rb {
-  err_file=/tmp/err_cn_s3_rb
-  ./cn s3 rb aaa >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 rb aaa
 }
 
 function s3_put {
-  err_file=/tmp/err_cn_s3_put
-  dd if=/dev/zero of=/tmp/ooo bs=1048576 count=10
-  ./cn s3 put ooo aaa >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file" /tmp/ooo
+  dd if=/dev/zero of=dd_file bs=1048576 count=10 &>/dev/null || fatal "Cannot run dd"
+  runCn s3 put dd_file aaa
+  deleteFile dd_file
 }
 
 function s3_get {
-  err_file=/tmp/err_cn_s3_get
-  ./cn s3 get aaa/ooo iii >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file" /tmp/iii
+  runCn s3 get aaa/dd_file get_file
+  deleteFile  get_file
 }
 
 function s3_ls {
-  err_file=/tmp/err_cn_s3_ls
-  ./cn s3 ls aaa >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 ls aaa
 }
 
 function s3_la {
-  err_file=/tmp/err_cn_s3_la
-  ./cn s3 la >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 la
 }
 
 function s3_info {
-  err_file=/tmp/err_cn_s3_info
-  ./cn s3 info aaa/ooo >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 info aaa/dd_file
 }
 
 function s3_du {
-  err_file=/tmp/err_cn_s3_du
-  ./cn s3 du aaa/ooo >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 du aaa/dd_file
 }
 
 function s3_mv {
-  err_file=/tmp/err_cn_s3_mv
-  ./cn s3 mv aaa/ooo aaa/uuu >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 mv aaa/dd_file aaa/dd_file2
 }
 
 function s3_sync {
-  err_file=/tmp/err_cn_s3_sync
-  ./cn s3 sync /tmp aaa >"$err_file" 2>&1
-  if [[ "$?" -eq 0 ]]; then
-    echo "${FUNCNAME[0]}: SUCCESS"
-  else
-    echo "${FUNCNAME[0]}: ERROR"
-    cat "$err_file"
-    exit 1
-  fi
-  rm -f "$err_file"
+  runCn s3 sync $tmp_dir aaa
 }
 
 function main(){
-version
-update
-purge
-logs
-restart
-status
-stop
-start
-version
-update
-status
-logs
+  version
+  update
+  purge
+  logs
+  restart
+  status
+  stop
+  start
+  version
+  update
+  status
+  logs
 
-s3_mb
-s3_rb
-s3_mb
-s3_put
-s3_get
-s3_ls
-s3_la
-s3_info
-s3_du
-s3_mv
-s3_sync
+  s3_mb
+  s3_rb
+  s3_mb
+  s3_put
+  s3_get
+  s3_ls
+  s3_la
+  s3_info
+  s3_du
+  s3_mv
+  s3_sync
 
-## rb all
+  ## rb all
 
-restart
+  restart
 
-# s3 again
+  # s3 again
 
-status
-version
-update
-purge
+  status
+  version
+  update
+  purge
 }
-
 
 main

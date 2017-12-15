bdfe1e5aeac339da12088dda51f3eccc352f27f2
Erwan Velu <erwan@redhat.com>
2017-12-15 13:50:34 +0100

    tests: Creating reportSuccess() function
    The actual code is considering reporting a success or failure based on
    the result of a single 'cn' command.
    
    runCn is now saving the return code into the runCnStatus variable and
    let the user make a call to reportSuccess() once the testing function is done.
    
    The error handling is now implicit : if a command fails the error handling is automatically engaged.
    
    This will allow more complex tests by running several cn commands before returning a success.

--- tests/functional-tests.sh
+++ tests/functional-tests.sh
@@ -4,6 +4,8 @@ err_file=""
 tmp_dir=/tmp
 bucket=mybucket
 file=dd_file
+runCnStatus=0
+lastTest=""
 
 function fatal() {
   echo "$@"
@@ -26,73 +28,106 @@ function deleteFile() {
   fi
 }
 
-function runCn() {
-  caller=${FUNCNAME[1]}
-  err_file=$(getTempFile $caller)
-  ./cn "$@" &>"$err_file"
-  if [[ "$?" -eq 0 ]]; then
-    printf '%-20s : SUCCESS\n' ${caller}
+function success {
+  printf '%-20s : SUCCESS\n' ${lastTest}
+}
+
+function failed() {
+  printf '%-20s : ERROR\n' ${lastTest}
+  fatal
+}
+
+function reportSuccess {
+  if [ -n "$1" ]; then
+    returnCode=$1
   else
-    printf '%-20s : ERROR\n' ${caller}
-    fatal
-    exit 1
+    returnCode=$runCnStatus
   fi
+
+  if [[ "$returnCode" -eq 0 ]]; then
+    success
+  else
+    failed
+  fi
+}
+
+function runCn() {
+  lastTest=${FUNCNAME[1]}
+  err_file=$(getTempFile $lastTest)
+  ./cn "$@" &>"$err_file"
+  runCnStatus=$?
   deleteFile $err_file
+  return $runCnStatus
+}
+
 }
 
 function test_start {
   runCn start -d $tmp_dir
+  reportSuccess
 }
 
 function test_help {
   runCn -h
+  reportSuccess
 }
 
 function test_stop {
   runCn stop
+  reportSuccess
 }
 
 function test_status {
   runCn status
+  reportSuccess
 }
 
 function test_restart {
   runCn restart
+  reportSuccess
 }
 
 function test_logs {
   runCn logs
+  reportSuccess
 }
 
 function test_purge {
   runCn purge --yes-i-am-sure
+  reportSuccess
 }
 
 function test_update {
   runCn update
+  reportSuccess
 }
 
 function test_version {
   runCn version
+  reportSuccess
 }
 
 function test_s3_mb {
   runCn s3 mb $bucket
+  reportSuccess
 }
 
 function test_s3_rb {
   runCn s3 rb $bucket
+  reportSuccess
 }
 
 function test_s3_put {
   dd if=/dev/zero of=${file} bs=1048576 count=10 &>/dev/null || fatal "Cannot run dd"
   runCn s3 put ${file} $bucket
   deleteFile ${file}
+  reportSuccess
 }
 
 function test_s3_get {
   runCn s3 get $bucket/${file} get_file
   deleteFile get_file
+  reportSuccess
 }
 
 function test_s3_del {
@@ -101,33 +136,43 @@ function test_s3_del {
   else
     runCn s3 del $1
   fi
+  reportSuccess
 }
 
 function test_s3_ls {
   runCn s3 ls $bucket
+  reportSuccess
 }
 
 function test_s3_la {
   runCn s3 la
+  reportSuccess
 }
 
 function test_s3_info {
   runCn s3 info $bucket/${file}
+  reportSuccess
 }
 
 function test_s3_du {
   runCn s3 du $bucket/${file}
+  reportSuccess
 }
 
 function test_s3_mv {
   runCn s3 mv $bucket/${file} $bucket/${file}.new
+  reportSuccess
+}
 }
 
 function test_s3_sync {
   runCn s3 sync $tmp_dir $bucket
+  reportSuccess
 }
 
 function main() {
+  set -e
+  trap failed 0
   for test in version update purge logs restart status stop start version update status logs; do
     test_$test
   done
@@ -146,6 +191,7 @@ function main() {
   for test in status version update purge; do
     test_$test
   done
+  trap - 0
 }
 
 main

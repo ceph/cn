ceph/cn

commit 6601f20bd867a1c7d9acd2ed56e26f8c4f4bdf31

s3: fix ls and la calls
Prior to this commit the output was incorrect and also the message "on
cluster $cluster" wasn't approprieate so I removed it.

Now 'ls' can receive a single argument only in order to list all the
buckets if empty where 'la' will continue to report all the objects in
all the buckets.

Signed-off-by: Sébastien Han <seb@redhat.com>

cmd/s3_la.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -26,13 +25,14 @@ func S3CmdLa(cmd *cobra.Command, args []string) {
 
 	notExistCheck(containerName)
 	notRunningCheck(containerName)
+
 	command := []string{"s3cmd", "la"}
-	output := strings.TrimSuffix(string(execContainer(containerName, command)), "\n") + " on cluster " + containerName
+	output := string(execContainer(containerName, command))
+
 	if len(output) == 1 {
 		command := []string{"s3cmd", "ls"}
-		output := strings.TrimSuffix(string(execContainer(containerName, command)), "\n") + " on cluster " + containerName
-		fmt.Println(output)
-	} else {
-		fmt.Println(output)
+		output := execContainer(containerName, command)
+		fmt.Printf("%s", output)
 	}
+	fmt.Printf("%s", output)
 }

cmd/s3_ls.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -10,9 +9,9 @@ import (
 // cliS3CmdLs is the Cobra CLI call
 func cliS3CmdLs() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "ls CLUSTER BUCKET",
+		Use:   "ls CLUSTER [BUCKET]",
 		Short: "List objects or buckets",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.RangeArgs(1, 2),
 		Run:   S3CmdLs,
 	}
 
@@ -25,7 +24,14 @@ func S3CmdLs(cmd *cobra.Command, args []string) {
 
 	notExistCheck(containerName)
 	notRunningCheck(containerName)
-	command := []string{"s3cmd", "ls", "s3://" + args[1]}
-	output := strings.TrimSuffix(string(execContainer(containerName, command)), "\n") + " on cluster " + containerName
-	fmt.Println(output)
+
+	var command []string
+
+	if len(args) == 1 {
+		command = []string{"s3cmd", "ls"}
+	} else {
+		command = []string{"s3cmd", "ls", "s3://" + args[1]}
+	}
+	output := execContainer(containerName, command)
+	fmt.Printf("%s", output)
 }

commit 388aa3639f0afeca30dfa951e1ac4ff5ed260ced

s3_la: Avoid double printing of execContainer()
Commit 6601f20bd867a1c7d9acd2ed56e26f8c4f4bdf31 introduced two issues :
- command & output are redefined in an if statement
- if len(output) == 1 then the Printf is done twice which is unexpected

This commit is about reusing command & output variables at the function
scope and make a single Print call for both cases.

This commit is also switching to Println() to match s3_ls but also
simplify the code : having a printf("%s") isn't really useful.

Signed-off-by: Erwan Velu <erwan@redhat.com>

cmd/s3_la.go
@@ -30,9 +30,8 @@ func S3CmdLa(cmd *cobra.Command, args []string) {
 	output := string(execContainer(containerName, command))
 
 	if len(output) == 1 {
-		command := []string{"s3cmd", "ls"}
-		output := execContainer(containerName, command)
-		fmt.Printf("%s", output)
+		command = []string{"s3cmd", "ls"}
+		output = execContainer(containerName, command)
 	}
-	fmt.Printf("%s", output)
+	fmt.Println(output)
 }